e0f9c99de5ebae9ffd220ec08e31fc0b9d9a0e22
pythonicforge <pythonicforge@gmail.com>
2025-04-07 09:52:16 +0530

    added: placeholder commands

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
-import cmd
-from typing import Optional
 import sys
+import cmd
+import builtins
+from termcolor import colored
+from typing import Optional, Literal
 
 class Skillz(cmd.Cmd):
     os.system('clear')
@@ -13,19 +15,32 @@ class Skillz(cmd.Cmd):
 
     def do_show(self, arg:str) -> None:
         """Shows the skill chart."""
-        pass
+        self.print("DASHBOARD\n", "light_blue")
 
     def do_add(self, arg:str) -> None:
         """Add a new language and it's respective skill level: add <lang> <skill-level>"""
-        pass
+        try:
+            language, skillLevel = arg.split()
+            self.print(f'[INFO] {language} added at a skill level of {skillLevel}\n',"light_green")
+        except ValueError as e:
+            self.print(f"[ERROR] {e}\n", "red")
 
     def do_update(self, arg:str) -> None:
         """Update an existing and it's respective skill level: update <lang> <skill-level>"""
-        pass
+        try:
+            language, skillLevel = arg.split()
+            self.print(f'[INFO] {language} updated at a skill level of {skillLevel}\n',"light_green")
+        except ValueError as e:
+            self.print(f"[ERROR] {e}\n", "red")
 
     def do_bye(self, arg:str) -> None:
         """Exits the program."""
-        pass
+        self.print(f'Bye 👋\n',"light_cyan")
+        sys.exit(0)
+
+    def print(self, text:str, color:Optional[Literal['black', 'grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'light_grey', 'dark_grey', 'light_red', 'light_green', 'light_yellow', 'light_blue', 'light_magenta', 'light_cyan', 'white']]) -> None:
+        """Modified print function that prints text with colors"""
+        builtins.print(colored(text, color))
 
 if __name__ == '__main__':
     try:
